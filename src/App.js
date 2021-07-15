3755196ab196fc82115300b869b48eb5d144d193
Guilherme-Vecchi <guivecchi12@gmail.com>
2021-07-15 16:05:04 -0600

    figure out how to load dogs when page loads

--- src/App.js
+++ src/App.js
@@ -7,27 +7,56 @@ import './App.css';
 function App() {
   const [dogImage, setDogImage] = useState();
   const [dogs, setDogs] = useState();
+  const [searchDog, setSearchDog] = useState();
+  const [myDog, setMyDog] = useState();
   useEffect(()=>{
     axios
       .get('https://api.thedogapi.com/v1/breeds?limit=175&page=0')
       .then(res => {
         setDogs(res.data)
-        console.log("DOGS: ", dogs)
+        console.log("DOGS: ", dogs[0])
         setDogImage(dogs[0].image.url)
       })
       .catch(err => console.log(err))
       // eslint-disable-next-line
   },[])
+
+  const formSubmit = (e) => {
+    e.preventDefault();
+
+    // Search Dogs for specific dog
+    if(dogs !== undefined){
+      dogs.forEach(dog => {
+        if(myDog in dog.name){
+          console.log(dog)
+        }
+      })
+      // Delete Search
+      setSearchDog("")
+    }    
+  } 
   
+  const handleChange = (e) => {
+    setSearchDog(e.target.value)
+  } 
+
   return (
     <div className="mainDiv">
       <h1 id="header">All about Dogs!</h1>
+      <form onSubmit={formSubmit}>
+        <label>
+          <input type="text" value={searchDog} name="searchDog" onChange = {handleChange}/>
+        </label>        
+        <input type="submit" value="Submit" className="searchButton"/>
+      </form>
       <div className="dogImage">
-        Image
         {dogImage ? <img src= {dogImage} alt = {dogImage}></img> : <div>No Image</div>}
       </div>
       <div>
-        Information about the breed: {dogs[0].name}
+        Information about the breed:
+        {dogImage ? <div>{dogs[0].name}</div> : <div>Loading</div>}
+         
+
       </div>
     </div>
   );
