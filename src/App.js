09ebdaac2a943bf3783795bb87b69b9ef6d35305
Guilherme-Vecchi <guivecchi12@gmail.com>
2021-07-14 22:44:02 -0600

    used information from API

--- src/App.js
+++ src/App.js
@@ -6,20 +6,26 @@ import './App.css';
 
 function App() {
   const [dogImage, setDogImage] = useState();
+  const [dogs, setDogs] = useState();
   useEffect(()=>{
     axios
-      .get('https://api.thedogapi.com/v1/images/search')
+      .get('https://api.thedogapi.com/v1/breeds?limit=200&page=0')
       .then(res => {
-        console.log(res);
-        setDogImage(res.data[0].url)
+        setDogs(res.data)
+        console.log(dogs[0])
+        setDogImage(dogs[0].image.url)
       })
       .catch(err => console.log(err))
   },[])
+  
   return (
     <div className="mainDiv">
       <h1 id="header">All about Dogs!</h1>
-      <div>
+      <div className="dogImage">
         <img src= {dogImage} alt = {dogImage}></img>
+      </div>
+      <div>
+        Information about the breed: {dogs[0].name}
       </div>
     </div>
   );
