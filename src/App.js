2bbb9c606db2c46a9f89f491225348aac92d3550
Guilherme-Vecchi <guivecchi12@gmail.com>
2021-07-16 14:29:05 -0600

    finished project

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-
-
 function App() {
   const [dogs, setDogs] = useState();
   const [searchDog, setSearchDog] = useState('');
   const [myDog, setMyDog] = useState();
   var found = false;
   useEffect(()=>{
+    // Create an Array with all Dogs
     if(!dogs){
       axios
         .get('https://api.thedogapi.com/v1/breeds?limit=175&page=0')
@@ -29,9 +28,10 @@ function App() {
     // Search Dogs for specific dog
     dogs.forEach(dog => {
       // check for word within the name, example: Labrador within Labrador Retriever
-      if(dog.name.toLowerCase().split(' ').includes(searchDog.toLocaleLowerCase())){
+      if(dog.name.toLowerCase().split(' ').includes(searchDog.toLocaleLowerCase()) || dog.name === searchDog){
         found = true
         setMyDog(dog)
+        console.log(myDog)
       }
     })
 
@@ -52,29 +52,33 @@ function App() {
   return (
     <div className="mainDiv">
       <h1 id="header">All about Dogs!</h1>
-      <form onSubmit={formSubmit}>
-        <label>
-          <input type="text" value={searchDog} name="searchDog" onChange = {handleChange}/>
-        </label>        
-        <input type="submit" value="Submit" className="searchButton"/>
+      <form className="search"> 
+        <input type="text" value={searchDog} name="searchDog" className= "searchBox" onChange = {handleChange}/>
+        <button className="searchButton" onClick={formSubmit}>
+          Search
+        </button>  
       </form>
-      <div className="dogImage">
-        { !myDog ? 'Loading . . .' : <img src= {myDog.image.url} alt = {myDog.name}></img>}
-      </div>
-      <div>
-        <h2>Information about the breed:</h2>
-        { !myDog ? 
-        " Loading . . ." :
-        <div>
-          <h3>{myDog.name}</h3>
-          <h4>Origin: {myDog.origin}</h4>
-          <h5>Weight: {myDog.weight.imperial} lbs </h5>
-          <h5> Height: {myDog.height.imperial} inches</h5>
-          <h5>Temperament: </h5>
-          <p>{myDog.temperament}</p>
-        </div> 
-        }
-      </div>
+      <div className="myDog">
+        <div className="dogImage">
+          { !myDog ? 'Loading . . .' : <img src= {myDog.image.url} alt = {myDog.name}></img>}
+        </div>
+        <div className ="information">
+          <h1 className="informationHeader">Information about the breed:</h1>
+          { !myDog ? 
+          " Loading . . ." :
+          <div className="information">
+            <h2 id="dogName">{myDog.name}</h2>
+            <h3 id="origin">Origin: <span className="dogInfo">{myDog.origin}</span></h3>
+            <h3 id="origin">Bred for: <span className="dogInfo">{myDog.bred_for}</span></h3>
+            <h3 id="origin">Breed group: <span className="dogInfo">{myDog.breed_group}</span></h3>
+            <h3 id="weight">Weight: <span className="dogInfo">{myDog.weight.imperial} lbs</span> </h3>
+            <h3 id="height"> Height: <span className="dogInfo">{myDog.height.imperial} inches</span></h3>
+            <h3 id='temperamentHeader'>Temperament: </h3>
+            <p id='temperamentBody'>{myDog.temperament}</p>
+          </div>
+          }
+        </div>
+      </div> 
     </div>
   );
 }
